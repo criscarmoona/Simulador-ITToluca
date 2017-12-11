9d7110c765658c880324a260c32e52c380728ecf
Erick Hernandez <ehernandez@macroaplicaciones.com>
2017-12-10 22:33:02 -0600

    Hora con multiplicador

--- Simulador/Simulador/Logica/Generador.cs
+++ Simulador/Simulador/Logica/Generador.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Windows.System.Threading;
-using Windows.UI.Xaml;
 
 namespace Simulador.Logica
 {
@@ -11,22 +8,40 @@ namespace Simulador.Logica
         public EventHandler<Persona> NuevaPersona { get; set; }
         public EventHandler<Camion> NuevoCamion { get; set; }
         public EventHandler<int> QuitarCamion { get; set; }
-        public EventHandler<string> NuevaHora { get; set; }
+        public EventHandler<DateTime> NuevaHora { get; set; }
 
-        ThreadPoolTimer dispatcherTimer;
-
-        private int TiempoParadaCamionMilisegundos = 5;
+        ThreadPoolTimer temporizador;
+        private int multiplicadorTiempo;
+        public DateTime horaSimulador = new DateTime(2017, 12, 9, 6, 0, 0);
 
         public Generador()
         {
-            IniciarTiempo();
+            IniciarSimulador();
+
         }
-        public void IniciarTiempo()
+        public void IniciarSimulador()
         {
-           
+            multiplicadorTiempo = 1;
+            temporizador = ThreadPoolTimer.CreatePeriodicTimer(cambioHora, TimeSpan.FromMilliseconds(CalcularPeriodo()));
         }
-        void dispatcherTimer_Tick(ThreadPoolTimer timer)
+        void cambioHora(ThreadPoolTimer timer)
         {
+            horaSimulador = horaSimulador.AddMinutes(1);
+            NuevaHora?.Invoke(null, horaSimulador);
+            Imprimir(horaSimulador.ToString("hh:mm:ss tt") + " multiplicador: " + multiplicadorTiempo);
+        }
+
+        public void CambiarVelocidad(double multiplicador)
+        {
+            multiplicadorTiempo = Convert.ToInt32(multiplicador);
+            temporizador.Cancel();
+            temporizador = ThreadPoolTimer.CreatePeriodicTimer(cambioHora, TimeSpan.FromMilliseconds(CalcularPeriodo()));
+        }
+        private double CalcularPeriodo()
+        {
+            double nuevoPeriodo = 1000 / multiplicadorTiempo;
+            Imprimir("Periodo: " + nuevoPeriodo);
+            return nuevoPeriodo;
         }
         private void Imprimir(string texto)
         {
